f0b7a9c5d42fb155558056256296d778c309f150
Dino <dinohossain1@gmail.com>
2024-10-05 22:43:30 +0100

    Landing page skeleton

---
+++ app/(marketing)/footer.tsx
@@ -0,0 +1,8 @@
+export const Footer = () => {
+	return (
+		// Hides footer on small screens, shows on large screens (lg:block)
+		<footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
+			Footer
+		</footer>
+	);
+};
