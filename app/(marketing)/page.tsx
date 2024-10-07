fde952155c7d4a6cb146e903b42ed7f6e0964c0a
Dino <dinohossain1@gmail.com>
2024-10-07 17:40:23 +0100

    Landing page completed

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -10,6 +10,7 @@ import {
 import { Loader } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+
 export default function Home() {
 	return (
 		<div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
@@ -18,33 +19,35 @@ export default function Home() {
 			</div>
 			<div className="flex flex-col items-center gap-y-8">
 				<h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
-					Learn, practice, and master new languages with Lingo.
+					Learn, practice, and master new languages with Duolingo.
 				</h1>
+				<div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
+					<ClerkLoading>
+						<Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+					</ClerkLoading>
+					<ClerkLoaded>
+						<SignedOut>
+							<SignUpButton mode="modal">
+								<Button size="lg" variant="secondary" className="w-full">
+									Get Started
+								</Button>
+							</SignUpButton>
+
+							<SignInButton mode="modal">
+								<Button size="lg" variant="primaryOutline" className="w-full">
+									I already have an account
+								</Button>
+							</SignInButton>
+						</SignedOut>
+
+						<SignedIn>
+							<Button size="lg" variant="secondary" className="w-full" asChild>
+								<Link href="/learn">Continue Learning</Link>
+							</Button>
+						</SignedIn>
+					</ClerkLoaded>
+				</div>
 			</div>
-			<ClerkLoading>
-				<Loader className="h-5 w-5 text-muted-foreground animate-spin" />
-			</ClerkLoading>
-			<ClerkLoaded>
-				<SignedOut>
-					<SignUpButton mode="modal">
-						<Button size="lg" variant="secondary" className="w-full">
-							Get Started
-						</Button>
-					</SignUpButton>
-
-					<SignInButton mode="modal">
-						<Button size="lg" variant="primaryOutline" className="w-full">
-							I already have an account
-						</Button>
-					</SignInButton>
-				</SignedOut>
-
-				<SignedIn>
-					<Button size="lg" variant="secondary" className="w-full" asChild>
-						<Link href="/learn">Continue Learning</Link>
-					</Button>
-				</SignedIn>
-			</ClerkLoaded>
 		</div>
 	);
 }
